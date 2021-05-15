ee4b26aab5e8cbe56e35d8af50cf5b702a802250
md-ummar <ummarkhan98@gmail.com>
2021-05-16 01:27:21 +0530

    new code

---
+++ code.js
@@ -0,0 +1,10 @@
+const quoteGen = document.getElementById("button");
+let par = document.querySelector("p");
+quoteGen.addEventListener("click",()=>{
+    par.textContent = "I should learn more programming";
+
+}) 
+
+
+
+
